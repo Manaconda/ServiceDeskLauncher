3d2e17b172fb00a33145fd439fe28b8f49ae1a65
Manaconda <andy0taylor@gmail.com>
2015-09-23 21:39:55 +0100

    V1.1 - Patched to run on systems running .net 4 / 4.6

--- ServiceDeskLauncher/My Project/AssemblyInfo.vb
+++ ServiceDeskLauncher/My Project/AssemblyInfo.vb
@@ -18,7 +18,7 @@ Imports System.Runtime.InteropServices
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("0511bb84-ed21-4163-a9a6-543973a1db24")> 
+<Assembly: Guid("0511bb84-ed21-4163-a9a6-543973a1db24")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: AssemblyVersion("1.1.0.0")>
+<Assembly: AssemblyFileVersion("1.1.0.0")>
